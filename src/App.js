375b779b6a3c11a31917cdd7d1832394ff0ef9b2
israr <mdisrar164@gmail.com>
2023-02-21 01:42:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = () => {
+    if (newTodo.trim() === '') {
+      return;
+    }
+    setTodos([...todos, { title: newTodo, completed: false }]);
+    setNewTodo('');
+  };
+
+  const removeTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
+  const toggleCompleted = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (index, newTitle) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, title: newTitle } : todo
+      )
+    );
+  };
+
+  const clearCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleCompleted(index)}
+            />
+            <span
+              onDoubleClick={() => {
+                const newTitle = prompt('Enter a new title:', todo.title);
+                if (newTitle !== null) {
+                  editTodo(index, newTitle);
+                }
+              }}
+              style={{ textDecoration: todo.completed ? 'line-through' : '' }}
+            >
+              {todo.title}
+            </span>
+            <button onClick={() => removeTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={clearCompleted}>Clear Completed</button>
     </div>
   );
 }
